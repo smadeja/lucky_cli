53ee2dd5f30f72394a22514146252c3dcdb9dff7
Szymon Madeja <smadeja1@gmail.com>
2019-06-21 19:29:25 +0200

    Use the current timestamp instead of '00000000000001'
    as the version of the user migration in the authentication app skeleton.

--- src/lucky_cli/base_authentication_src_template.cr
+++ src/lucky_cli/base_authentication_src_template.cr
@@ -1,3 +1,9 @@
 class BaseAuthenticationSrcTemplate < Teeplate::FileTree
   directory "#{__DIR__}/../base_authentication_app_skeleton"
+
+  @version : String
+
+  def initialize
+    @version = Time.utc.to_s("%Y%m%d%H%M%S")
+  end
 end
